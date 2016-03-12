51e56b31a27472584608064045c3c9cd45484a0b
This_Is_Amir <arabiamir13@gmail.com>
2016-03-11 22:36:03 -0800

    Added files via upload

---
+++ data/config.lua
@@ -0,0 +1,23 @@
+do local _ = {
+  disabled_channels = {},
+  enabled_plugins = {
+    "eur",
+    "echo",
+    "btc",
+    "giphy",
+    "google",
+    "gps",
+    "help",
+    "location",
+    "Plugins",
+    "Set",
+    "version",
+    "weather",
+    "Id"
+  },
+  sudo_users = {
+    139172429
+  }
+}
+return _
+end
